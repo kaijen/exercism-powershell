c1fcf40f4da1898ca23b6ab34a0c92298f614954
Kai Jendrian <kai.jendrian@secorvo.de>
2023-12-29 15:08:31 +0100

    Reworked resistor-color-duo

--- resistor-color-duo/ResistorColorDuo.ps1
+++ resistor-color-duo/ResistorColorDuo.ps1
@@ -1,6 +1,51 @@
-. ..\resistor-color\ResistorColor.ps1
+Function Get-ColorCode() {
+    <#
+    .SYNOPSIS
+    Translate a color to its corresponding color code.
 
-#endregion
+    .DESCRIPTION
+    Given a color, return its corresponding color code.
+
+    .PARAMETER Color
+    The color to translate to its corresponding color code.
+
+    .EXAMPLE
+    Get-ColorCode -Color "black"
+    #>
+    [CmdletBinding()]
+    Param(
+        [string]$Color
+    )
+
+    $Color = $Color.ToLower()
+    
+    [array] $colors = Get-Colors
+
+    [bool] $color_defined = $colors.Contains($Color)
+
+    if ( -not $color_defined ) {
+        throw $Color + " is not defined!"
+    }
+
+    return $colors.IndexOf($Color)
+}
+
+Function Get-Colors() {
+    <#
+    .SYNOPSIS
+    Return the list of all colors.
+
+    .DESCRIPTION
+    Return the list of all colors.
+
+    .EXAMPLE
+    Get-Colors
+    #>
+    
+    [array] $colors = "black",  "brown",  "red",  "orange",  "yellow",  "green",  "blue",  "violet",  "grey",  "white"
+        
+    return $colors
+}
 
 Function Get-ColorCodeValue() {
     <#
