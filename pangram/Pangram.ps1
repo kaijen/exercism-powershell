e5d108f387a927ab68a6a09cfbc4c5c727cb5e22
Kai Jendrian <kai.jendrian@secorvo.de>
2024-01-03 14:15:55 +0100

    Solved Pangram

--- pangram/Pangram.ps1
+++ pangram/Pangram.ps1
@@ -2,16 +2,16 @@ Function Invoke-Panagram() {
     <#
     .SYNOPSIS
     Determine if a sentence is a pangram.
-    
+
     .DESCRIPTION
     A pangram is a sentence using every letter of the alphabet at least once.
-    
+
     .PARAMETER Sentence
     The sentence to check
-    
+
     .EXAMPLE
     Invoke-Panagram -Sentence "The quick brown fox jumps over the lazy dog"
-    
+
     Returns: $true
     #>
     [CmdletBinding()]
@@ -19,5 +19,15 @@ Function Invoke-Panagram() {
         [string]$Sentence
     )
 
-    Throw "Please implement this function"
+    $alphabet = [ordered]@{}
+    [char]'a'..[char]'z' | Foreach-Object  {
+        $alphabet[$_] = 0
+    }
+
+    $Sentence.ToLower().ToCharArray() | ForEach-Object {
+        $alphabet[$_]++
+    }
+
+    return ( $alphabet.Values | Measure-Object -Minimum).Minimum -gt 0
+
 }
