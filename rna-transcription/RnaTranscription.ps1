9dc2c56551543bbd2a8af67af19312814bf3eeb4
Kai Jendrian <kai.jendrian@secorvo.de>
2024-01-05 13:43:29 +0100

    Reworked RnaTranscription

--- rna-transcription/RnaTranscription.ps1
+++ rna-transcription/RnaTranscription.ps1
@@ -17,12 +17,17 @@ Function Invoke-RnaTranscription() {
         [string]$Strand
     )
 
-    $transcription = @{ G="C"; C="G"; T="A"; A="U" }
+    $dna = [string[]]@("G", "C", "T", "A")
+    $rna = [string[]]@("C", "G", "A", "U")
+    $combine = [Func[string, string, [string[]]]] { @($args[0], $args[1]) }
+    $transcription = @{}
+
+    # Wanted to see if there is a Python like ZIP in Powershell ;-)
+    [Linq.Enumerable]::Zip($dna,$rna,$combine) | ForEach-Object { $transcription.Add($_[0],$_[1]) }
 
     if ( ( $Strand.ToUpper() -Replace "[GCTA]","" ).Length -ne 0 ) {
         Throw "Not a vald strand"
     }
 
     return ( $Strand.ToUpper() -Split "" | ForEach-Object { $_ -Replace $_, $transcription[$_] } ) -join ""
-
 }
