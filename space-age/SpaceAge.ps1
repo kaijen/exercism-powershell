48bb3b79f9d202b1465add4b8577fa7b510ecc02
Kai Jendrian <kai.jendrian@secorvo.de>
2024-01-11 14:51:05 +0100

    Reworked Space Age

--- space-age/SpaceAge.ps1
+++ space-age/SpaceAge.ps1
@@ -1,3 +1,13 @@
+$conversions = @{
+    Mercury = 0.2408467
+    Venus   = 0.61519726
+    Earth   = 1.0
+    Mars    = 1.8808158
+    Jupiter = 11.862615
+    Saturn  = 29.447498
+    Uranus  = 84.016846
+    Neptune = 164.79132
+}
 
 Function Get-SpaceAge() {
     <#
@@ -22,23 +32,12 @@ Function Get-SpaceAge() {
     [CmdletBinding()]
     Param(
         [int]$Seconds,
-        [ValidateSet(
-            "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune",
+        [ValidateScript(
+            { $conversions.ContainsKey($_) },
             ErrorMessage = "Invalid planet"
         )]
         [string]$Planet = "Earth"
     )
-
-    $conversions = @{
-        Mercury = 0.2408467
-        Venus   = 0.61519726
-        Earth   = 1.0
-        Mars    = 1.8808158
-        Jupiter = 11.862615
-        Saturn  = 29.447498
-        Uranus  = 84.016846
-        Neptune = 164.79132
-    }
 
     $earth_seconds = 31557600
 
